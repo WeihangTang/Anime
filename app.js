113e401a4c7bd9cc6f812062cc778d7b457622bd
Weihang Tang <tangweihang806@gmail.com>
2018-10-22 22:42:07 +0000

    connected to mongodb

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/animes', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
 
-var animes = [
-    {name:"Death Note", image:"https://myanimelist.cdn-dena.com/r/50x70/images/anime/9/9453.webp?s=bcf651aae2cd301a32bcc46e317a98bc"},
-    {name:"Shingeki no Kyojin", image:"https://myanimelist.cdn-dena.com/r/50x70/images/anime/10/47347.webp?s=6b6f0445f6a93276f3cbd98400582612"}
-    ];
+var animeSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   rating: String
+});
+
+var animes = mongoose.model("animes", animeSchema);
+
+animes.create(
+    {
+    name: "No Game No Life",
+    image: "https://myanimelist.cdn-dena.com/r/50x70/images/anime/5/65187.webp?s=fd1048e7e02d97f7183eb7c5f0c39e70",
+    description: "",
+    rating: "8.39"
+    }, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("new anime created");
+        }
+    }
+);
+
+// var animes = [
+//     {name:"Death Note", image:"https://myanimelist.cdn-dena.com/r/50x70/images/anime/9/9453.webp?s=bcf651aae2cd301a32bcc46e317a98bc"},
+//     {name:"Shingeki no Kyojin", image:"https://myanimelist.cdn-dena.com/r/50x70/images/anime/10/47347.webp?s=6b6f0445f6a93276f3cbd98400582612"}
+//     ];
 
 app.get("/",function(req, res){
    //res.send("this is your landing page"); 
-   res.render("index");
+   res.render("landing");
+  
 });
 
 app.get("/animes", function(req, res){
-   res.render("animes", {animes:animes});
+   //res.render("animes", {animes:animes});
+    animes.find({}, function(err, animes){
+       if(err){
+           console.log(err);
+       }else{
+           res.render("index",{animes:animes});
+       }
+   });
 });
 
 
